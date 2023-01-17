31c49fc10580bad3e265c41b7cdaf8b2a087c462
Pagwin <git@pagwin.xyz>
2023-01-17 00:59:14 -0500

    playing with sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
 extern crate serde;
+extern crate tokio;
+extern crate rocket;
+extern crate sqlx;
+extern crate lazy_static;
 mod schema;
+
+use sqlx::prelude::*;
+use lazy_static::lazy_static;
+use tokio::runtime::Runtime;
+
+lazy_static!{
+    static ref RUNTIME:Runtime = Runtime::new().expect("Async runtime creation failed");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x:Option<u8> = Some(3);
+    let y:<Option::<u8> as IntoIterator>::Item = x.unwrap();
+    //RUNTIME.block_on(async {
+    //    let mut conn = sqlx::postgres::PgConnectOptions::new()
+    //        .host("localhost")
+    //        .username("sqlx")
+    //        //once I'm actually getting to work I should probably do this with an env var or a key
+    //        //store of some kind so I'm not just yeeting the password into the binary
+    //        .password(include_str!("postgres_passwd"))
+    //        .port(5432)
+    //        .database("sqlx")
+    //        .application_name("sqlx_test")
+    //        .connect().await
+    //        .expect("Connection failed");
+    //    sqlx::query!("SELECT asdf as id").execute(&mut conn).await.unwrap();
+    //});
+    //println!("Hello, world!");
 }
--- src/schema.rs
+++ src/schema.rs
@@ -2,6 +2,7 @@ use std::rc;
 
 //Not sure if using rc is the best choice atm or if I want to have the Rcs hold onto refcells but
 //whatever
+#[derive(Clone)]
 pub struct TaskV1{
     title:String,
     body:String,
@@ -13,6 +14,7 @@ pub struct TaskV1{
     /// Child nodes
     children:Vec<rc::Rc<TaskV1>>
 }
+
 pub enum TaskVersioning{V1(TaskV1)}
 
 /// Trait that any method of encoding and decoding tasks needs to implement
@@ -21,11 +23,13 @@ pub trait TaskEncoder{
     /// encode_task and if a value of it is gotten that way then should be usable with decode_task
     /// to retrieve the original task it must be serializable with serde due to it being the value
     /// passed around when working with tasks potentially onto disk or over network
-    type Identifier:serde::Serialize + serde::Deserialize;
+    //Specifying DeserializeOwned may be a problem in the future if I need to deal with types with
+    //lifetimes but until then this is good
+    type Identifier:serde::Serialize + serde::de::DeserializeOwned;
     type EncodingError;
     type DecodingError;
     type IdentityFetchError;
-    fn encode_task(&mut self, task:TaskVersioning)->Result<Self::Identifier,Self::EncodingError>;
-    fn decode_task(&mut self,id:Self::Identifier)->Result<TaskVersioning,Self::DecodingError>;
-    fn provide_identifiers(&mut self)->Result<Vec<Self::Identifier>,Self::IdentityFetchError>;
+    fn encode_task<'a>(&mut self, task:TaskVersioning)->Result<Self::Identifier,Self::EncodingError>;
+    fn decode_task<'a>(&mut self,id:Self::Identifier)->Result<TaskVersioning,Self::DecodingError>;
+    fn provide_identifiers<'a>(&mut self)->Result<Vec<Self::Identifier>,Self::IdentityFetchError>;
 }
