c6ed40e101e4854a010ecb7241177d108d43dc4a
Pagwin <git@pagwin.xyz>
2023-01-18 12:08:27 -0500

    can now fetch media with a task

--- src/sql_impl.rs
+++ src/sql_impl.rs
@@ -1,11 +1,9 @@
 use crate::task::*;
 use sqlx::{query,query_as};
-use sqlx::{Encode,Decode};
+use sqlx::{Row,Column};
 use uuid::Uuid;
 use async_trait::async_trait;
 
-
-#[derive(sqlx::Encode, sqlx::Decode)]
 struct TaskV1Schema{
     id: Uuid,
     title:String,
@@ -58,8 +56,35 @@ impl TaskEncoder for sqlx::Pool<sqlx::postgres::Postgres> {
             }
         }
     }
-    async fn decode_task(&mut self, id:Uuid)->Result<TaskVersioning,sqlx::Error>{
-        
+    async fn decode_task(&mut self, id:Uuid)->Result<Option<TaskVersioning>,sqlx::Error>{
+        let potential_task = query!("SELECT * FROM pogo_tasks WHERE id=$1 AND title IS NOT NULL AND body IS NOT NULL AND progress IS NOT NULL", id).fetch_optional(self as &Self).await?.map(|s|{
+                // unsafe is for unwrap_unchecked which is fine because the SQL query already does
+                // the checking for us
+                unsafe {
+                    TaskVersioning::V1(TaskV1{
+                        id:s.id.unwrap_unchecked(),
+                        title:s.title.unwrap_unchecked(),
+                        body:s.body.unwrap_unchecked(),
+                        progress:s.progress.unwrap_unchecked(),
+                        media:Vec::new(),
+                        parents: Vec::new(),
+                        children: Vec::new()
+                    })
+                }
+            }
+        );
+        match potential_task {
+            Some(TaskVersioning::V1(mut task))=>{
+                let media_ids = query!("SELECT resource_id FROM pogo_resource_mapping WHERE task_id=$1 AND resource_id IS NOT NULL",task.id).fetch_all(self as &Self).await?.into_iter().map(|r|unsafe{r.resource_id.unwrap_unchecked()}).collect::<Vec<Uuid>>();
+                let media:Vec<String> = query!("SELECT location FROM pogo_resources WHERE id = ANY($1) AND location IS NOT NULL",media_ids.as_slice())
+                    .fetch_all(self as &Self).await?.into_iter()
+                    .map(|row|row.location.unwrap_unchecked())
+                    .collect();
+                task.media = media;
+                Ok(Some(TaskVersioning::V1(task)))
+            },
+            None=>Ok(None)
+        }
     }
     async fn provide_identifiers(&mut self)->Result<Vec<Uuid>,sqlx::Error>{
         query_as!(Id,"SELECT id FROM pogo_tasks")
